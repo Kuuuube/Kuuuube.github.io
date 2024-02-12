6abdf6ee11c05bedc0082838021e986ae2cee2fe
kuuuube <hexagonisalie@gmail.com>
2024-02-11 20:30:45 -0500

    Add kuuube's_wacom_nib_compatibility_mastersheet

--- update_sheets.bat
+++ update_sheets.bat
@@ -13,6 +13,11 @@ curl -L "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtV
 curl -L "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?format=ods" > ./kuuube's_wacom_tablet_pen_compatibility_mastersheet.ods
 curl -L "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?format=pdf" > ./kuuube's_wacom_tablet_pen_compatibility_mastersheet.pdf
 
+curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/htmlview" > ./kuuube's_wacom_nib_compatibility_mastersheet.html
+curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=xlsx" > ./kuuube's_wacom_nib_compatibility_mastersheet.xlsx
+curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=ods" > ./kuuube's_wacom_nib_compatibility_mastersheet.ods
+curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=pdf" > ./kuuube's_wacom_nib_compatibility_mastersheet.pdf
+
 git add .
 git commit -m "Update sheets"
 git push
