6c200c64eea60999795d8c8f71522b7e9a001a5c
kuuuube <hexagonisalie@gmail.com>
2024-03-08 18:50:47 -0500

    Add shell script for updating

---
+++ update_sheets.sh
@@ -0,0 +1,23 @@
+curl -L "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/htmlview" > "./kuuube's_wacom_tablet_mastersheet.html"
+curl -L "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/export?format=xlsx" > "./kuuube's_wacom_tablet_mastersheet.xlsx"
+curl -L "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/export?format=ods" > "./kuuube's_wacom_tablet_mastersheet.ods"
+curl -L "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/export?format=pdf" > "./kuuube's_wacom_tablet_mastersheet.pdf"
+
+curl -L "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/htmlview" > "./kuuube's_tablet_buying_guide.html"
+curl -L "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/export?format=xlsx" > "./kuuube's_tablet_buying_guide.xlsx"
+curl -L "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/export?format=ods" > "./kuuube's_tablet_buying_guide.ods"
+curl -L "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/export?format=pdf" > "./kuuube's_tablet_buying_guide.pdf"
+
+curl -L "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/htmlview" > "./kuuube's_wacom_tablet_pen_compatibility_mastersheet.html"
+curl -L "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?format=xlsx" > "./kuuube's_wacom_tablet_pen_compatibility_mastersheet.xlsx"
+curl -L "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?format=ods" > "./kuuube's_wacom_tablet_pen_compatibility_mastersheet.ods"
+curl -L "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?format=pdf" > "./kuuube's_wacom_tablet_pen_compatibility_mastersheet.pdf"
+
+curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/htmlview" > "./kuuube's_wacom_nib_compatibility_mastersheet.html"
+curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=xlsx" > "./kuuube's_wacom_nib_compatibility_mastersheet.xlsx"
+curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=ods" > "./kuuube's_wacom_nib_compatibility_mastersheet.ods"
+curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=pdf" > "./kuuube's_wacom_nib_compatibility_mastersheet.pdf"
+
+git add .
+git commit -m "Update sheets"
+git push
