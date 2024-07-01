4472dba7cdd9173403ed64eeae165f9622470198
kuuuube <hexagonisalie@gmail.com>
2024-07-01 12:04:29 -0400

    Fix pdf

--- yomitan-pdf-viewer/update.sh
+++ yomitan-pdf-viewer/update.sh
@@ -4,6 +4,6 @@ wget "https://github.com/mozilla/pdf.js/releases/download/v$pdfjs_version/pdfjs-
 unzip -o "pdfjs-$pdfjs_version-dist.zip" "web/*" -d "."
 unzip -o "pdfjs-$pdfjs_version-dist.zip" "build/*" -d "."
 mv "./web/viewer.html" "./web/index.html"
-cp "./.deploy_assets/yomitan-pdf-viewer.pdf" "./web/compressed.tracemonkey-pldi-09.pdf"
+cp "yomitan-pdf-viewer.pdf" "./web/compressed.tracemonkey-pldi-09.pdf"
 
 rm "pdfjs-$pdfjs_version-dist.zip"
