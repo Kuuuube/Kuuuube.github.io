8ff914d79484fd117122f89df2cb1864df097d34
kuuuube <hexagonisalie@gmail.com>
2024-11-27 14:04:29 -0500

    Add make intentionals to update script

--- confusables-finder/update.sh
+++ confusables-finder/update.sh
@@ -1,3 +1,4 @@
 curl https://www.unicode.org/Public/security/latest/confusables.txt > confusables.txt
 curl https://www.unicode.org/Public/security/latest/intentional.txt > intentional.txt
 python make_confusables_js.py
+python make_intentionals_js.py
