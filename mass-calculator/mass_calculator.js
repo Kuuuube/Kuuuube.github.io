511c7d61a8156832f8bcfcaae6c72ece1e5396d8
kuuuube <hexagonisalie@gmail.com>
2025-07-31 16:52:38 -0400

    Add octagonal prism

--- mass-calculator/mass_calculator.js
+++ mass-calculator/mass_calculator.js
@@ -145,6 +145,14 @@ function calculate_mass() {
             result_mass_kg = volume_meters * mean_density;
             break;
         }
+        case "octagonal_prism": {
+            const octagon_edge_length = convert_to_meters(document.querySelector("#octagonal_prism_octagon_edge_length").value, size_source_unit);
+            const prism_height = convert_to_meters(document.querySelector("#octagonal_prism_prism_height").value, size_source_unit);
+            const octagon_apothem = octagon_edge_length / (2 * Math.tan(22.5 * Math.PI / 180));
+            const volume_meters = 4 * octagon_apothem * octagon_edge_length * prism_height;
+            result_mass_kg = volume_meters * mean_density;
+            break;
+        }
         case "octahedron": {
             const edge_length = convert_to_meters(document.querySelector("#octahedron_edge_length").value, size_source_unit);
             const volume_meters = Math.sqrt(2) / 3 * Math.pow(edge_length, 3);
