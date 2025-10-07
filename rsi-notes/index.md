62fb69e25efe9a83eec12a903d430a033516e4eb
kuuuube <hexagonisalie@gmail.com>
2025-10-07 19:37:14 -0400

    Start of rsi notes page

---
+++ rsi-notes/index.md
@@ -0,0 +1,14 @@
+# Kuuube's RSI Notes
+
+This is a collection of tips that have helped me through dealing with RSIs (Repetitive Strain Injuries) over 5+ years.
+
+If you find this information valuable and would like to support me I have a [ko-fi](https://ko-fi.com/Kuuube) and I'm an Amazon affiliate (if you click this link before purchasing something, doesn't matter what, I will get a small percent: [https://amzn.to/4g74yQP](https://amzn.to/4g74yQP)).
+
+I may mention specific brand names or link to products. I have no affiliation to any brand mentioned here. **All links past this point are not affiliate or sponsored in any way.**
+
+## Finding Neutral Wrist Angle
+
+1. Place your forearm flat on a table.
+2. Make a loose fist with your thumb to the side (not under your hand)
+
+This is roughly where your wrist is at a neutral angle. Note that the top of your knuckles are above your wrist.
---
+++ rsi-notes/render.py
@@ -0,0 +1,25 @@
+import comrak
+
+def get_html_head(output_html_path):
+    replacements = [
+        {"target": r"{page_title}", "replacement": "Kuuube's RSI Notes"},
+        {"target": r"{styles}", "replacement": open("./styles.css").read()},
+    ]
+    head_html = open("./head.html").read()
+    for replacement in replacements:
+        head_html = head_html.replace(replacement["target"], replacement["replacement"])
+    return head_html
+
+def markdown_to_html(markdown_string):
+    return comrak.render_markdown(markdown_string)
+
+def render_html_page(output_html_path, markdown_data):
+    output_html = ""
+    output_html += get_html_head(output_html_path)
+    output_html += "<body>\n"
+    output_html += markdown_to_html(markdown_data)
+    output_html += "</body>\n"
+    return output_html
+
+with open("index.html", "w", encoding = "utf8") as output_html:
+    output_html.write(render_html_page("../rsi-notes/index.html", open("index.md").read()))
