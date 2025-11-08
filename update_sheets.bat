4266083516605529cabe92134e76c6e60d45ceef
kuuuube <hexagonisalie@gmail.com>
2025-11-08 17:33:34 -0500

    Update scripts

--- update_sheets.bat
+++ update_sheets.bat
@@ -1,22 +1,22 @@
-curl -L "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/export?/format=zip" > ./kuuube's_wacom_tablet_mastersheet.zip
-curl -L "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/export?format=xlsx" > ./kuuube's_wacom_tablet_mastersheet.xlsx
-curl -L "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/export?format=ods" > ./kuuube's_wacom_tablet_mastersheet.ods
-curl -L "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/export?format=pdf" > ./kuuube's_wacom_tablet_mastersheet.pdf
+curl -L -o "./kuuube's_wacom_tablet_mastersheet.zip" "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/export?format=zip"
+curl -L -o "./kuuube's_wacom_tablet_mastersheet.xlsx" "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/export?format=xlsx"
+curl -L -o "./kuuube's_wacom_tablet_mastersheet.ods" "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/export?format=ods"
+curl -L -o "./kuuube's_wacom_tablet_mastersheet.pdf" "https://docs.google.com/spreadsheets/d/125LNzGmidy1gagwYUt12tRhrNdrWFHhWon7kxWY7iWU/export?format=pdf"
 
-curl -L "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/export?/format=zip" > ./kuuube's_tablet_buying_guide.zip
-curl -L "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/export?format=xlsx" > ./kuuube's_tablet_buying_guide.xlsx
-curl -L "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/export?format=ods" > ./kuuube's_tablet_buying_guide.ods
-curl -L "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/export?format=pdf" > ./kuuube's_tablet_buying_guide.pdf
+curl -L -o "./kuuube's_tablet_buying_guide.zip" "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/export?format=zip"
+curl -L -o "./kuuube's_tablet_buying_guide.xlsx" "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/export?format=xlsx"
+curl -L -o "./kuuube's_tablet_buying_guide.ods" "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/export?format=ods"
+curl -L -o "./kuuube's_tablet_buying_guide.pdf" "https://docs.google.com/spreadsheets/d/1DYVfiSpQqdpa4sWWYUALPmliOIuGyKog7B7LJJdmlhE/export?format=pdf"
 
-curl -L "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?/format=zip" > ./kuuube's_wacom_tablet_pen_compatibility_mastersheet.zip
-curl -L "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?format=xlsx" > ./kuuube's_wacom_tablet_pen_compatibility_mastersheet.xlsx
-curl -L "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?format=ods" > ./kuuube's_wacom_tablet_pen_compatibility_mastersheet.ods
-curl -L "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?format=pdf" > ./kuuube's_wacom_tablet_pen_compatibility_mastersheet.pdf
+curl -L -o "./kuuube's_wacom_tablet_pen_compatibility_mastersheet.zip" "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?/format=zip"
+curl -L -o "./kuuube's_wacom_tablet_pen_compatibility_mastersheet.xlsx" "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?format=xlsx"
+curl -L -o "./kuuube's_wacom_tablet_pen_compatibility_mastersheet.ods" "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?format=ods"
+curl -L -o "./kuuube's_wacom_tablet_pen_compatibility_mastersheet.pdf" "https://docs.google.com/spreadsheets/d/1UAFPjGj0ThthPBvOSVVwOm08HW3qWtVoP1iFBwLMyPI/export?format=pdf"
 
-curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?/format=zip" > ./kuuube's_wacom_nib_compatibility_mastersheet.zip
-curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=xlsx" > ./kuuube's_wacom_nib_compatibility_mastersheet.xlsx
-curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=ods" > ./kuuube's_wacom_nib_compatibility_mastersheet.ods
-curl -L "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=pdf" > ./kuuube's_wacom_nib_compatibility_mastersheet.pdf
+curl -L -o "./kuuube's_wacom_nib_compatibility_mastersheet.zip" "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?/format=zip"
+curl -L -o "./kuuube's_wacom_nib_compatibility_mastersheet.xlsx" "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=xlsx"
+curl -L -o "./kuuube's_wacom_nib_compatibility_mastersheet.ods" "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=ods"
+curl -L -o "./kuuube's_wacom_nib_compatibility_mastersheet.pdf" "https://docs.google.com/spreadsheets/d/1EG_D5l6P3wyYUyiriqUzHiNSfFCIuv5KD67ahZLmNc0/export?format=pdf"
 
 git add .
 git commit -m "Update sheets"
